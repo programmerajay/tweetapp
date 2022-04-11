2fe58b34186690c90672f361d236a532db0f1bfe
Ajay I <49202488+programmerajay@users.noreply.github.com>
2022-04-11 18:33:52 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports={
     
-        consumer_key:         "IJklGB3WF5gn0yD0TePkaOGEC",
-        consumer_secret:      'Nr9MHbKybWbEnv6480rV1BysvNDqid0xV2FB3fj4MVeVncNJry',
-        access_token:         '791148205543190529-1k0JKbcmGLQEjZ227Kc0gpZQesGipYP',
-        access_token_secret:  'oe0KLjMdPjT2zOGFuWugMgmcP3AtuocMqIZo13pNFmNfS'
+        consumer_key:         "",
+        consumer_secret:      '',
+        access_token:         '',
+        access_token_secret:  ''
     }
